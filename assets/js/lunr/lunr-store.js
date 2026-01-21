2491f56be2a486ce2edafa45842b43c6c2264b56
HwaYeong Lee <lhy2128@naver.com>
2026-01-21 13:59:57 +0000

    오늘 자 업데이트 dracolich-kr/private-git-blog@1cfaa36aa7bb4ddde5c0334db400d4c86ca0ecd7

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -6,7 +6,7 @@ var store = [{
         "teaser": null
       },{
         "title": "[MySql] MySql 이란?",
-        "excerpt":"개요 RDB(Relational Database) 관계형 데이터 베이스이다. MySql은 스토리지 엔진에 따라 다양한 데이터 저장 방식을 지원 대표적으로 MyISAM, InnoDB 가 존재 MyISAM InnoDB가 나오기전에 사용되었던 기본 스토리지 엔진 사용 방법이 간편하지만 트랜잭션이 불가능하고 데이터가 삭제가 되었을 때 불필요한 데이터(OverHead)가 발생하여 현재는 InnoDB가 기본 스토리지 엔진으로 사용되고 있다. InnoDB MySql의 기본 스토리지...","categories": ["MySql"],
+        "excerpt":"개요 1995년도에 발표된 RDBS(Relational Database System) 관계형 데이터 베이스이다. MySql은 스토리지 엔진에 따라 다양한 데이터 저장 방식을 지원 대표적으로 MyISAM, InnoDB 가 존재 한다. MyISAM InnoDB가 나오기전에 사용되었던 기본 스토리지 엔진 사용 방법이 간편하지만 트랜잭션이 불가능하고 데이터가 삭제가 되었을 때 불필요한 데이터(OverHead)가 발생하여 현재는 InnoDB가 기본 스토리지 엔진으로 사용되고 있다....","categories": ["MySql"],
         "tags": ["Database","MySql"],
         "url": "/mysql/mysql/",
         "teaser": null
@@ -15,5 +15,17 @@ var store = [{
         "excerpt":"   GitHub Blog에서 줄바꿈 적용 방법      &lt;br&gt;를 입력해 줄바꿈 처리를 한다.   맨 끝줄에 스페이스바를 두번 입력한다.  ","categories": ["GitBlog"],
         "tags": ["Git","GitBlog"],
         "url": "/gitblog/Tip/",
+        "teaser": null
+      },{
+        "title": "[MySql] MySql Replication",
+        "excerpt":"   Replication 이란?  MySql의 운영을 할 때에는 기본적으로 복제(Replication)를 세팅을 하여 관리를 한다.  복제를 통해 몇가지 장점을 얻을 수 있다.      고가용성을 확보하여 데이터베이스의 안정성을 확보.   Write (Master) Read (Slave)를 구분하여 데이터베이스의 분산   부하가 있는 작업은 Slave에서 처리를 하여 Master에 부하를 감소   등의 성능을 확보 할 수 있다.   ","categories": ["MySql"],
+        "tags": ["Database","MySql"],
+        "url": "/mysql/mysql-replication/",
+        "teaser": null
+      },{
+        "title": "C# 프로젝트 폴더가 다른 위치에 있는 cs파일 참조 By Global Using",
+        "excerpt":"기존 using 문의 경우 개별 파일마다 개별로 선언을 해서처리를 해야해서 매우 불편하고 사용하기 힘든 단점이 많은 기능이다. 하지만 C# 10에 Global Using이 추가 되면서 이젠 프로젝트 단위로 하나의 cs 파일에 선언을 하면 다른 파일에서도 별도의 선언을 할 필요 없이 편하게 사용을 할 수 있게 되었다. 주로 맴버 타입을 재정의 하는...","categories": ["MySql"],
+        "tags": ["Database","MySql"],
+        "url": "/mysql/tip/",
         "teaser": null
       }]
